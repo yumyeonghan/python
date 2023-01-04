0cd638c0ced6181ac86ba62a49092e43fc8e6a12
yumyeonghan <mgtmh991013@gmail.com>
2023-01-04 17:53:16 +0900

    구현(시뮬레이션)

---
+++ implementation/ex01.py
@@ -0,0 +1,50 @@
+# 게임 개발
+n, m = map(int,input().split())
+x, y, direction = map(int, input().split())
+data = []
+for _ in range(m):
+    data.append(list(map(int, input().split())))
+
+data[x][y] = 1
+count = 1
+turn_time = 0
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def turn_left(direction):
+    index = direction - 1
+    if index == -1:
+        index = 3
+    return index
+
+while True:
+    direction = turn_left(direction)
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    
+    if data[nx][ny] != 1:
+        x = nx
+        y = ny
+        data[x][y] = 1
+        count += 1
+        turn_time = 0
+        continue
+    
+    else:
+        turn_time += 1
+    
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        if data[nx][ny] != 1:
+            x = nx
+            y = ny
+            data[x][y]
+            count+= 1
+            turn_time = 0
+            continue
+        else:
+            break
+
+print(count)
