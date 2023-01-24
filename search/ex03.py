03e22e0c7751d9462fb81ed9ba1a5e100f662d93
yumyeonghan <mgtmh991013@gmail.com>
2023-01-25 00:11:50 +0900

    다시 푼 문제

--- search/ex03.py
+++ search/ex03.py
@@ -3,25 +3,16 @@ from collections import deque
 
 n, k = map(int, input().split())
 data = []
-for _ in range(n):
-    data.append(list(map(int, input().split())))
-s, x, y = map(int, input().split())
-
-빈칸_수 = 0
 virus = []
 for i in range(n):
+    data.append(list(map(int, input().split())))
     for j in range(n):
-        if data[i][j] == 1:
-            virus.append((1, 0, i, j))
-        if data[i][j] == 2:
-            virus.append((2, 0, i, j))
-        if data[i][j] == 3:
-            virus.append((3, 0, i, j))
-
+        if data[i][j] != 0:
+            virus.append((data[i][j], 0, i, j))
+            
+s, x, y = map(int, input().split())
 virus.sort()
 q = deque()
-
-print(virus)
 
 for i in virus:
     q.append(i)
@@ -30,11 +21,9 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 while q:
-    
-    
-
     종류, time, 행, 열 = q.popleft()
-    if time == s:
+    
+    if s == time:
         break
     
     for i in range(4):
